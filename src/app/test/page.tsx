5f8ebdf582690e7ca154d21ccd5542ef39e2e419
solomonnealfoster <solomonnealfoster@gmail.com>
2025-03-31 21:32:51 -0400

    Contraband Test Film

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,13 +5,12 @@ import getStarted from '/videos/get-started.mp4';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-1 sm:items-start">
-        Hello world
-        <Video src="https://stream.mux.com/sxY31L6Opl02RWPpm3Gro9XTe7fRHBjs92x93kiB1vpc.m3u8" />;
+      <main className="padding-top">
+        <Video src="https://stream.mux.com/g8VVMXEcD4rzVm8jYtQHCyZBXTI4yEGt6y6hcrFRJYQ.m3u8" />
+          Contraband (Short Film - Test)
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        
       </footer>
     </div>
-  );
+  )
 }
