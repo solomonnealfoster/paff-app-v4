b253408f9b3844a37fd971572804af2dad8d741f
solomonnealfoster <solomonnealfoster@gmail.com>
2025-04-12 15:12:44 -0400

    Install Tailwinds Modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+
 import Image from "next/image";
 
 export default function Home() {
+
+function test(){
+  alert("Testing");
+}
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -48,6 +55,21 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             Read our docs
+          </a>
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            onClick={test}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Test Function
           </a>
         </div>
       </main>
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,16 +1,62 @@
+'use client'
+
+import {useRef, useEffect} from 'react';
 import Image from "next/image";
 import Video from 'next-video';
 import getStarted from '/videos/get-started.mp4';
 
-export default function Home() {
-  return (
+const Home = () => {
+  const ref = useRef(null);
+
+  function onTimeUpdate(event) {
+    console.log(event);
+    console.log(this.currentTime);
+    }
+
+      function onPause(event) {
+    console.log("PAUSED");
+    }
+
+  useEffect(() => {
+    const handleClick = event => {
+      console.log('Button clicked');
+
+      console.log('bobbyhadz.com');
+      console.log(this)
+    };
+
+     const playEvent = event => {
+      console.log('PLAYING');
+    };
+
+    const element = ref.current;
+
+    element.addEventListener('click', handleClick);
+      element.addEventListener('play', playEvent);
+    return () => {
+      element.removeEventListener('click', handleClick);
+    };
+
+
+  
+  }, []);
+
+  return  (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="padding-top">
-        <Video src="https://stream.mux.com/g8VVMXEcD4rzVm8jYtQHCyZBXTI4yEGt6y6hcrFRJYQ.m3u8" />
-          Contraband (Short Film - Test)
+       <button ref={ref}>Click</button>
+        <Video 
+            src="https://stream.mux.com/g8VVMXEcD4rzVm8jYtQHCyZBXTI4yEGt6y6hcrFRJYQ.m3u8" 
+            onTimeUpdate={onTimeUpdate}
+            onPause={onPause}
+        />
+          Contraband (Short Film - Test #2)
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
+
