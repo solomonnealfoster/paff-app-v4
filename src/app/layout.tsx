150c684ffea938993ebdf1e0f37fea1f750e2a8a
solomonnealfoster <solomonnealfoster@gmail.com>
2025-04-20 13:13:43 -0400

    Install Tailwinds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import BaseLayout from "@/layouts/BaseLayout.astro";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
